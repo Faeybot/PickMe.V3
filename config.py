f0364d09789853eb1859e064ea70092b435ccf09
Faeybot <faisal.siria@gmail.com>
2026-03-14 21:36:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-FEED_CHANNEL = os.getenv("FEED_CHANNEL")
-ADMIN_CHANNEL = os.getenv("ADMIN_CHANNEL")
+TOKEN = os.getenv("BOT_TOKEN")  # Telegram Bot Token
+DATABASE_URL = os.getenv("DATABASE_URL")  # PostgreSQL URL dari Railway
+
+FEED_LIMIT_TEXT = 3
+FEED_LIMIT_PHOTO = 1
+SWIPE_LIMIT = 20
+
+FEED_CHANNEL = os.getenv("FEED_CHANNEL")  # Channel feed ID (privat admin)
+ADMIN_CHANNEL = os.getenv("ADMIN_CHANNEL")  # Channel admin untuk block/report
